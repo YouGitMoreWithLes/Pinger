cc36ecf24fcb1de872114c6b45ada07d24c0a486
lmcwhirt-insight <86177157+lmcwhirt-insight@users.noreply.github.com>
2023-02-20 13:33:33 -0700

    Added proxy ability

--- Controllers/PingTestController.cs
+++ Controllers/PingTestController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Dynamic;
+using System.Net;
 using System.Net.NetworkInformation;
 using System.Text;
 
@@ -19,21 +21,21 @@ public class PingTestController : ControllerBase
     [Route("/health")]
     public async Task<string> Get()
     {
-        return "Healthy";
+        return await Task.FromResult("Healthy");
     }
 
     [HttpGet("Ping")]
-    public string Get([FromQuery] string hostName = "www.google.com", [FromQuery] int tries = 4)
+    public string Get([FromQuery] string hostName = "www.google.com", [FromQuery] int tries = 1, [FromQuery] int timeOut = 5)
     {
         string result = "Default string value";
 
         try
         {
-            StringBuilder stringBuilder= new StringBuilder();
+            StringBuilder stringBuilder = new StringBuilder();
             for (int i = 0; i < tries; i++)
             {
                 Ping myPing = new Ping();
-                PingReply reply = myPing.Send(hostName, 1000 * 15);
+                PingReply reply = myPing.Send(hostName, 1000 * timeOut);
 
                 if (reply != null)
                 {
@@ -47,16 +49,17 @@ public class PingTestController : ControllerBase
                 result = stringBuilder.ToString();
             }
         }
-        catch
+        catch (Exception ex)
         {
-            Console.WriteLine("ERROR: You have Some TIMEOUT issue");
+            Console.WriteLine(ex.GetBaseException().Message);
+            result = ex.GetBaseException().Message;
         }
 
         return result;
     }
 
     [HttpGet("GetHttpContent")]
-    public async Task<string> GetHttpContent([FromQuery] string url)
+    public async Task<string> GetHttpContent([FromQuery] string url = "https://www.google.com")
     {
         _logger.LogInformation("GetHttpContent: starting");
         string result = "Default value";
@@ -98,9 +101,52 @@ public class PingTestController : ControllerBase
         return result;
     }
 
+    [HttpGet("GetHttpContentWithProxy")]
+    public async Task<string> GetHttpContentWithProxy([FromQuery] string url = "https://www.google.com")
+    {
+        _logger.LogInformation("GetHttpContent: starting");
+        string result = "Default value";
+
+        try
+        {
+            HttpClient client = GetProxyHttpClient();
+            client.Timeout = new TimeSpan(0, 0, 15);
+
+            _logger.LogInformation($"GetHttpContent: GET: {url}");
+
+            var data = await client.GetAsync(url, new HttpCompletionOption()
+            {
+
+            });
+            _logger.LogInformation("GetHttpContent: executed http GET");
+
+            if (data.IsSuccessStatusCode)
+            {
+                _logger.LogInformation("GetHttpContent: GET was successful");
+
+                Uri hostUrl = new Uri(url);
+                var hmmm = CheckHostDns(hostUrl.Host);
+                result = await data.Content.ReadAsStringAsync();
+
+                _logger.LogInformation($"GetHttpContent: DNS result: {hmmm}");
+
+                result = hmmm + " : " + result;
+            }
+        }
+        catch (Exception exception)
+        {
+            _logger.LogError($"GetHttpContent: Exception: {exception.GetBaseException().Message}");
+            result = exception.GetBaseException().Message;
+        }
+
+        _logger.LogInformation($"GetHttpContent: result: {result}");
+
+        return result;
+    }
+
 
     [HttpGet("CheckHostDns")]
-    public string CheckHostDns([FromQuery] string hostName)
+    public string CheckHostDns([FromQuery] string hostName = "www.google.com")
     {
         _logger.LogInformation($"CheckHostDns: starting");
         string result = "Default string";
@@ -128,5 +174,26 @@ public class PingTestController : ControllerBase
         _logger.LogInformation($"CheckHostDns: starting");
 
         return result;
+    }
+
+    private HttpClient GetProxyHttpClient()
+    {
+        var proxy = new WebProxy
+        {
+            Address = new Uri($"http://proxy_cgw.sdsheriff.com:8080"),
+            BypassProxyOnLocal = true,
+            UseDefaultCredentials = false,
+        };
+
+        var httpClientHandler = new HttpClientHandler
+        {
+            Proxy = proxy,
+        };
+
+        httpClientHandler.ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+
+        var client = new HttpClient(handler: httpClientHandler, disposeHandler: true);
+
+        return client;
     }
 }
